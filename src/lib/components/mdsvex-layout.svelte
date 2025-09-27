8f270da9acce40fb665169a39fbb14d4df462a69
Corentin Merot <cmerot@themarqueeblink.com>
2025-09-27 11:58:45 +0200

    feat: convert some pages to svx

---
+++ src/lib/components/mdsvex-layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import PageTitle from '$lib/components/page-title/page-title.svelte';
+	import PageSubtitle from './page-title/page-subtitle.svelte';
+
+	// These props come from the frontmatter
+	let { children, title, description, page_title, page_subtitle } = $props();
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	{#if description}
+		<meta name="description" content={description} />
+	{/if}
+</svelte:head>
+
+<header>
+	<PageTitle>{page_title || title}</PageTitle>
+	{#if page_subtitle}
+		<PageSubtitle>{page_subtitle}</PageSubtitle>
+	{/if}
+</header>
+
+<main>
+	<article class="prose max-w-full">
+		{@render children()}
+	</article>
+</main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			layout: dirname(fileURLToPath(import.meta.url)) + '/src/lib/components/mdsvex-layout.svelte'
+		})
+	],
 	kit: {
 		adapter: adapter({
 			images: {
